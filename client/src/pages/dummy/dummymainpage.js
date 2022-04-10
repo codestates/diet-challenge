3b759a7b5d6acf848ce39d5901d650a2f691b324
kaisiok <okop10@nate.com>
2022-04-10 13:42:08 +0900

    더미데이터 생성

---
+++ client/src/pages/dummy/dummymainpage.js
@@ -0,0 +1,18 @@
+export const dummy = {
+  userinfo: {
+    id: "1",
+    userid: "test",
+    usernickname: "hyeonmin",
+    goal: "하루에 4km걷기",
+  },
+  latest: { img: "img", info: "info" },
+  friends: [
+    { nickname: "kimcoding", requset: true },
+    { nickname: "parkhacker", requset: true },
+    { nickname: "jordan", requset: true },
+    { nickname: "messi", requset: true },
+    { nickname: "musk", requset: false },
+    { nickname: "ronaldo", requset: false },
+  ],
+  friendslatest: [],
+};
