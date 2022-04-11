ebb0acfffe949803846d570c2b2383f625a81b20
dbstjs95 <choji95@naver.com>
2022-04-12 05:12:34 +0900

    배포 테스트: start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/diet-challenge/server
+
+export DATABASE_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USERNAME --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DB_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DB_PORT --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+
 authbind --deep pm2 start bin/www
