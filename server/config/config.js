8828e2a104d8947642d3cd353f5651361849f4d9
dbstjs95 <choji95@naver.com>
2022-04-12 12:46:47 +0900

    환경변수 없앰

--- server/config/config.js
+++ server/config/config.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DB_PORT,
+    username: "admin",
+    password: "ccbj5555",
+    database: "diet_challenge",
+    host: "diet-challenge-deploy.ccwj70jnseyi.ap-northeast-2.rds.amazonaws.com",
+    port: "3306",
     dialect: "mysql",
   },
   test: {
