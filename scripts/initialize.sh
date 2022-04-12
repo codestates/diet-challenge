cb04ed83c45e457fadbfebc413c1cc8ffe600e9d
dbstjs95 <choji95@naver.com>
2022-04-12 11:59:30 +0900

    appsspec 수정, initialize.sh 수정

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -5,5 +5,5 @@ npm install pm2@latest -g
 sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
-sudo chown ubuntu /etc/authbind/byport/80
+sudo chown root /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
