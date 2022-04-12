e6d251f17ce70d1d229f659bca398a0b72a9f7bf
dbstjs95 <choji95@naver.com>
2022-04-12 13:17:00 +0900

    환경변수 추가

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
-// const env = process.env;
 
 module.exports = {
   development: {
-    username: "admin",
-    password: "ccbj5555",
-    database: "diet_challenge",
-    host: "diet-challenge-deploy.ccwj70jnseyi.ap-northeast-2.rds.amazonaws.com",
-    port: "3306",
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   test: {
