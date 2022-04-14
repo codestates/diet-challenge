3614301d561200e30a2491ac4b5cbf5bb4fba468
dbstjs95 <choji95@naver.com>
2022-04-14 12:41:20 +0900

    mainpage 수정

--- server/controllers/mainpage/index.js
+++ server/controllers/mainpage/index.js
@@ -3,58 +3,58 @@
 const { sequelize, post: postModel } = require("../../models");
 const { isAuthorized } = require("../tokenFunctions");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const userInfo = isAuthorized(req);
-    if (!userInfo)
-      return res.status(400).json({
-        data: null,
-        message: "invalid access token",
-      });
+  if (!userInfo)
+    return res.status(400).json({
+      data: null,
+      message: "invalid access token",
+    });
 
-    const getLatestPost = () =>
-      postModel.findOne({
-        where: { id: userInfo.latestPostId },
-        attributes: [
-          "id",
-          "goal",
-          "user_id",
-          ["photo", "img"],
-          ["content", "info"],
-        ],
-      });
+  const getLatestPost = () =>
+    postModel.findOne({
+      where: { id: userInfo.latestPostId },
+      attributes: [
+        "id",
+        "goal",
+        "user_id",
+        ["photo", "img"],
+        ["content", "info"],
+      ],
+    });
 
-    const getFriends = () =>
-      sequelize.query(
-        `SELECT f.id as tableId, f.user_id as friendId, f.request, u.userNickName as nickname FROM friends f JOIN users u ON f.user_id = u.id WHERE f.fUser_id = ${userInfo.id}`,
-        { type: sequelize.QueryTypes.SELECT }
-      );
+  const getFriends = () =>
+    sequelize.query(
+      `SELECT f.id as tableId, f.user_id as friendId, f.request, u.userNickName as nickname FROM friends f JOIN users u ON f.user_id = u.id WHERE f.fUser_id = ${userInfo.id}`,
+      { type: sequelize.QueryTypes.SELECT }
+    );
 
-    const getFriendsLatestPosts = () =>
-      sequelize.query(
-        `select id, user_id, content as info, photo as img, goal from posts where posts.id in 
+  const getFriendsLatestPosts = () =>
+    sequelize.query(
+      `select id, user_id, content as info, photo as img, goal from posts where posts.id in 
       (select latestPostId from users where users.id in 
       (select user_id from friends 
       where fUser_id = ${userInfo.id} and request = true))`,
-        { type: sequelize.QueryTypes.SELECT }
-      );
+      { type: sequelize.QueryTypes.SELECT }
+    );
 
-    try {
-      const [latestPost, friends, friendsLatestPosts] = await Promise.all([
-        getLatestPost(),
-        getFriends(),
-        getFriendsLatestPosts(),
-      ]);
+  try {
+    const [latestPost, friends, friendsLatestPosts] = await Promise.all([
+      getLatestPost(),
+      getFriends(),
+      getFriendsLatestPosts(),
+    ]);
 
-      res.status(200).json({
-        data: {
-          userInfo,
-          latest: latestPost,
-          friends,
-          friendslatest: friendsLatestPosts,
-        },
-        message: "ok",
-      });
-    } catch (err) {
-      res.status(500).json({ data: null, message: "server error" });
-    }
+    res.status(200).json({
+      data: {
+        userInfo,
+        latest: latestPost,
+        friends,
+        friendslatest: friendsLatestPosts,
+      },
+      message: "ok",
+    });
+  } catch (err) {
+    res.status(500).json({ data: null, message: "server error" });
+  }
 };
