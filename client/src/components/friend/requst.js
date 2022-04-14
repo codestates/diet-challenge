594c32a092ec5dab4b892f807a46d0e0749ed74b
dbstjs95 <choji95@naver.com>
2022-04-15 04:37:59 +0900

    클라충돌해결

--- client/src/components/friend/requst.js
+++ client/src/components/friend/requst.js
@@ -31,13 +31,8 @@ export const Requst = ({ name, id, tableid }) => {
     console.log(id);
     axios
       .post(
-<<<<<<< HEAD
-        `http://localhost:4000/friends/accept`,
-        { friendTableId: id, friend_users_id: "" }, //메인페이지에서 전달받은 친구 정보중 친구테이블 id, 친구의 users 테이블 id
-=======
         `${process.env.REACT_APP_API_URL}/friends/accept`,
         { friendTableId: tableid, friend_users_id: id }, //메인페이지에서 전달받은 친구 정보중 친구테이블 id, 친구의 users 테이블 id
->>>>>>> 3f7666ca2f8cc6114601c7914816395c97e4ecd1
         {
           headers: {
             Authorization: `Bearer ${accessToken}`,
