e9567f4fe12b6c2384c8a87681a244c14a77b765
dbstjs95 <choji95@naver.com>
2022-04-14 16:07:21 +0900

    디비 연결 부분 주석처리

--- server/migrations/20220408030835-associations.js
+++ server/migrations/20220408030835-associations.js
@@ -1,87 +1,87 @@
-"use strict";
+// "use strict";
 
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addConstraint("friends", {
-      fields: ["user_id"],
-      type: "foreign key",
-      name: "friend_user_reference_1",
-      references: {
-        table: "users",
-        field: "id",
-      },
-    });
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.addConstraint("friends", {
+//       fields: ["user_id"],
+//       type: "foreign key",
+//       name: "friend_user_reference_1",
+//       references: {
+//         table: "users",
+//         field: "id",
+//       },
+//     });
 
-    await queryInterface.addConstraint("friends", {
-      fields: ["fUser_id"],
-      type: "foreign key",
-      name: "friend_user_reference_2",
-      references: {
-        table: "users",
-        field: "id",
-      },
-    });
+//     await queryInterface.addConstraint("friends", {
+//       fields: ["fUser_id"],
+//       type: "foreign key",
+//       name: "friend_user_reference_2",
+//       references: {
+//         table: "users",
+//         field: "id",
+//       },
+//     });
 
-    await queryInterface.addConstraint("users", {
-      fields: ["latestPostId"],
-      type: "foreign key",
-      name: "user_post_reference_1",
-      references: {
-        table: "posts",
-        field: "id",
-      },
-    });
+//     await queryInterface.addConstraint("users", {
+//       fields: ["latestPostId"],
+//       type: "foreign key",
+//       name: "user_post_reference_1",
+//       references: {
+//         table: "posts",
+//         field: "id",
+//       },
+//     });
 
-    await queryInterface.addConstraint("posts", {
-      fields: ["user_id"],
-      type: "foreign key",
-      name: "post_user_reference_1",
-      references: {
-        table: "users",
-        field: "id",
-      },
-    });
-  },
+//     await queryInterface.addConstraint("posts", {
+//       fields: ["user_id"],
+//       type: "foreign key",
+//       name: "post_user_reference_1",
+//       references: {
+//         table: "users",
+//         field: "id",
+//       },
+//     });
+//   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.removeConstraint("friends", {
-      fields: ["user_id"],
-      type: "foreign key",
-      name: "friend_user_reference_1",
-      references: {
-        table: "users",
-        field: "id",
-      },
-    });
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.removeConstraint("friends", {
+//       fields: ["user_id"],
+//       type: "foreign key",
+//       name: "friend_user_reference_1",
+//       references: {
+//         table: "users",
+//         field: "id",
+//       },
+//     });
 
-    await queryInterface.removeConstraint("friends", {
-      fields: ["fUser_id"],
-      type: "foreign key",
-      name: "friend_user_reference_2",
-      references: {
-        table: "users",
-        field: "id",
-      },
-    });
+//     await queryInterface.removeConstraint("friends", {
+//       fields: ["fUser_id"],
+//       type: "foreign key",
+//       name: "friend_user_reference_2",
+//       references: {
+//         table: "users",
+//         field: "id",
+//       },
+//     });
 
-    await queryInterface.removeConstraint("users", {
-      fields: ["latestPostId"],
-      type: "foreign key",
-      name: "user_post_reference_1",
-      references: {
-        table: "posts",
-        field: "id",
-      },
-    });
+//     await queryInterface.removeConstraint("users", {
+//       fields: ["latestPostId"],
+//       type: "foreign key",
+//       name: "user_post_reference_1",
+//       references: {
+//         table: "posts",
+//         field: "id",
+//       },
+//     });
 
-    await queryInterface.removeConstraint("posts", {
-      fields: ["user_id"],
-      type: "foreign key",
-      name: "post_user_reference_1",
-      references: {
-        table: "users",
-        field: "id",
-      },
-    });
-  },
-};
+//     await queryInterface.removeConstraint("posts", {
+//       fields: ["user_id"],
+//       type: "foreign key",
+//       name: "post_user_reference_1",
+//       references: {
+//         table: "users",
+//         field: "id",
+//       },
+//     });
+//   },
+// };
